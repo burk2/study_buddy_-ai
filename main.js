a726639f7faf610ac5cc417507ee5193226a30a4
Nolin Masai <nomalisanei6@gmail.com>
2025-09-02 08:48:35 +0300

    Create main.js
    Handles pop up subscription

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById("getStartedBtn").addEventListener("click", () => {
+  // Show PayPal subscription popup
+  alert("💳 Subscribe for $20/month using PayPal to unlock full access!");
+  window.location.href = "subjects.html";
+});
